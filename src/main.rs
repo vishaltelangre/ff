9e3b6896ed04fe7d664f253678185c3b13490821
Vishal Telangre <the@vishaltelangre.com>
2019-02-23 00:57:37 +0530

    Colorize actual match in the printed file paths

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate clap;
 extern crate regex;
 extern crate walkdir;
 
-use ansi_term::Colour::Red;
+use ansi_term::Colour::{Green, Red};
 use atty::Stream;
 use clap::{crate_authors, crate_version, App};
 use regex::Regex;
@@ -22,24 +22,46 @@ fn main() {
 
     if let Some(pattern) = matches.value_of("PATTERN") {
         let formatted_pattern = format!(r#"{}"#, pattern).to_string();
-        match Regex::new(formatted_pattern.as_str()) {
+        match Regex::new(&formatted_pattern) {
             Ok(regex_pattern) => {
                 let walker = WalkDir::new(".").into_iter();
                 let ok_entries = walker.filter_map(|e| e.ok());
-                for entry in ok_entries
-                    .filter(|e| regex_pattern.is_match(e.path().display().to_string().as_str()))
+                for entry in
+                    ok_entries.filter(|e| regex_pattern.is_match(&e.path().display().to_string()))
                 {
-                    println!("{}", entry.path().display());
+                    let file_path = entry.path().display().to_string();
+
+                    if atty::isnt(Stream::Stdout) {
+                        println!("{}", file_path);
+
+                        process::exit(0);
+                    }
+
+                    match regex_pattern.find(&file_path) {
+                        Some(result) => {
+                            let matched_slice = &file_path[result.start()..result.end()];
+                            let colored_match = Green.bold().paint(matched_slice).to_string();
+                            let file_path = &file_path.replace(matched_slice, &colored_match);
+                            println!("{}", file_path);
+                        }
+                        None => {
+                            println!("{}", file_path);
+                        }
+                    }
                 }
             }
-            Err(_err) => {
-                let erronous_pattern = if atty::is(Stream::Stderr) {
+            Err(_) => {
+                let erroneous_pattern = if atty::is(Stream::Stderr) {
                     Red.paint(formatted_pattern).to_string()
                 } else {
                     formatted_pattern
                 };
 
-                eprintln!("Failed to parse the provided PATTERN: {}" , erronous_pattern);
+                eprintln!(
+                    "Failed to parse the provided PATTERN: {}",
+                    erroneous_pattern
+                );
+
                 process::exit(1);
             }
         }
