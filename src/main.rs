a66e89b7b22038286031679a1ee9cef246dbf028
Vishal Telangre <the@vishaltelangre.com>
2019-02-23 00:57:36 +0530

    Unwrap accessible files and filter matching ones

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-extern crate clap;
-extern crate walkdir;
 extern crate ansi_term;
+extern crate clap;
 extern crate regex;
+extern crate walkdir;
 
-use clap::{crate_authors, crate_version, App};
-use walkdir::WalkDir;
 use ansi_term::Colour::Red;
-use regex::{Regex};
+use clap::{crate_authors, crate_version, App};
+use regex::Regex;
 use std::process;
+use walkdir::{WalkDir};
 
 fn main() {
     let matches = App::new("ff")
@@ -23,14 +23,18 @@ fn main() {
         match Regex::new(formatted_pattern.as_str()) {
             Ok(regex_pattern) => {
                 let walker = WalkDir::new(".").into_iter();
-                for entry in walker.filter_map(|e| e.ok()) {
-                    if regex_pattern.is_match(entry.path().display().to_string().as_str()) {
-                        println!("{}", entry.path().display());
-                    }
+                let ok_entries = walker.filter_map(|e| e.ok());
+                for entry in ok_entries
+                    .filter(|e| regex_pattern.is_match(e.path().display().to_string().as_str()))
+                {
+                    println!("{}", entry.path().display());
                 }
-            },
-            Err(err) => {
-                println!("Failed to parse the provided PATTERN: {}", formatted_pattern);
+            }
+            Err(_err) => {
+                eprintln!(
+                    "Failed to parse the provided PATTERN: {}",
+                    formatted_pattern
+                );
                 process::exit(1);
             }
         }
